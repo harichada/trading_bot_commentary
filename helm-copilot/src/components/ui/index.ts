1a0159e631b17c87522552f4269302199007e6f1
harichada <harikishorereddy@gmail.com>
2026-09-14 14:23:37 -0400

    chore: drop accidental helm-copilot from inventory brief commit

--- helm-copilot/src/components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Badge'
-export * from './Card'
-export * from './Button'
--- helm-copilot/src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './useApi'
-export * from './useWebSocket'
-export * from './useConnectionStatus'
